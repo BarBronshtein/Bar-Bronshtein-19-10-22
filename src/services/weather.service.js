1f7150fff5d04be24098142a5b00e57a4c640403
BarBronshtein <barbronshtein@gmail.com>
2022-10-19 11:38:49 +0300

    added curWeather to weatherService

--- src/services/weather.service.js
+++ src/services/weather.service.js
@@ -2,28 +2,58 @@ import axios from 'axios';
 
 export const weatherService = {
 	cityAutoComplete,
+	curWeather,
 };
 const CACHE_KEY = 'cache';
-const cache = JSON.parse(localStorage.getItem(CACHE_KEY || 'null')) || {};
+const cache = JSON.parse(localStorage.getItem(CACHE_KEY || 'null')) || {
+	locations: {},
+	curWeather: {},
+};
 const API_KEY = process.env.REACT_APP_ACUWEATHER_API_KEY;
 
 async function cityAutoComplete(txt) {
+	const cacheKey = 'locations';
 	const baseUrl =
 		'http://dataservice.accuweather.com/locations/v1/cities/autocomplete';
 	const query = `?apikey=${API_KEY}&q=` + txt;
 	try {
-		if (cache[txt]) return cache[txt];
-		const res = await axios.get(baseUrl + query);
-		const locations = res.data.map(location => ({
+		if (cache[cacheKey][txt]) return cache[cacheKey][txt];
+		const { data } = await axios.get(baseUrl + query);
+		const locations = data.map(location => ({
 			city: location.LocalizedName,
 			country: location.Country.LocalizedName,
+			key: location.Key,
 		}));
-		cache[txt] = locations;
-		localStorage.setItem(CACHE_KEY, cache);
+		cache[cacheKey][txt] = locations;
+		localStorage.setItem(CACHE_KEY, JSON.stringify(cache));
+		return locations;
 	} catch (err) {
 		console.log(err);
 		throw new Error('Failed to get data please try again later');
 	}
 }
+async function curWeather(key) {
+	const cacheKey = 'curWeather';
+	const baseUrl = 'http://dataservice.accuweather.com/currentconditions/v1/';
+	const query = key + '?apikey=' + API_KEY;
+	if (cache[cacheKey][key]) return cache[cacheKey][key];
+	try {
+		const { data } = await axios.get(baseUrl + query);
+		const curCondition = {
+			text: data[0].WeatherText,
+			icon: data[0].WeatherIcon,
+			temperature: {
+				c: data[0].Temperature.Metric.Value,
+				f: data[0].Temperature.Imperial.Value,
+			},
+		};
 
+		cache[cacheKey][key] = curCondition;
+		localStorage.setItem(CACHE_KEY, JSON.stringify(cache));
+		return curCondition;
+	} catch (err) {
+		console.log(err);
+		throw new Error('Failed to get data please try again later');
+	}
+}
 window.weatherService = weatherService;
